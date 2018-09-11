1fe1efe61c51f21fd01b5c6f181d827a326ef88c
starrainjung <43143231+starrainjung@users.noreply.github.com>
2018-09-11 22:17:46 +0800

    homework1

---
+++ homework1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string s1, s2;
+        int a, b;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            s1 = textBox1.Text;
+            a = int.Parse(s1);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            s2 = textBox2.Text;
+            b = int.Parse(s2);
+        }
+
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string c = Convert.ToString(a * b);
+            this.textBox3.Text = c;
+        }
+
+       
+
+    }
+}
