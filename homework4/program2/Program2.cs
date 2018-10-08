db4291e35bc520ee7cf1b1ddaa19419124e9abfa
starrainjung <43143231+starrainjung@users.noreply.github.com>
2018-10-08 19:27:35 +0800

    第四次作业

---
+++ homework4/program2/Program2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework4_2
+{
+    class Program2
+    {
+        class Order {
+            
+            internal string OrderNumber;
+
+            internal string OrderName;
+
+            public Order(String OrderNumber, String OrderName = "")
+            {
+                this.OrderNumber = OrderNumber;
+                this.OrderName = OrderName;
+            }
+        }
+        class OrderDetails {
+            private string goodName;
+            private int goodNumber;
+            private double Price;
+
+            public OrderDetails(string goodName, int goodNumber, double singlePrice)
+            {
+                this.goodName = goodName;
+                this.goodNumber = goodNumber;
+                this.Price = singlePrice;
+            }
+        }
+        class OrderService {
+            /*1、用list储存订单数据
+             2、删除订单信息
+             3、查询（按不同类型）
+             4、显示
+             */
+
+            public static List<Order> orders = new List<Order>();
+
+            public static void delete(string OrderNumber)
+            {
+                bool Y = false;//判断是否有该订单号的订单
+                foreach (Order o in orders)
+                {
+                    if (o.OrderNumber == OrderNumber)
+                    {
+                        Y = true;
+                        orders.Remove(o);
+                        break;
+                    }
+                }
+                if (Y == false)
+                {
+                    Console.WriteLine("没有该订单号");
+                }
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            /*异常处理、*/
+        }
+
+
+    }
+}
