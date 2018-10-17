c0a9401bdcd6eeda1323e2114c55ecb5252b94f9
starrainjung <43143231+starrainjung@users.noreply.github.com>
2018-10-17 23:00:47 +0800

    第五次作业

---
+++ homework5/homework5_1/homework5/OrderService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+
+{
+    
+    class OrderService
+    {
+        public List<Order> orders = new List<Order>();
+       
+
+        //添加订单
+        public void add(Order od)
+        {
+            orders.Add(od);
+        }
+        //删除订单
+        public void DelOrder(Order order)
+        {
+            orders.Remove(order);
+        }
+
+        public void DeleteOrderByOrderNumber(string ordernum)                              //根据订单号删除订单
+        {
+            bool count = false;
+            for (int i = 0; i < orders.Count; i++)
+            {
+                if (ordernum == orders[i].orderNumber)
+                {
+                    orders.Remove(orders[i]);
+                    count=true;
+                }
+            }
+            if (count == false)
+            { throw new CanNotFindOrder("没有该订单！"); } 
+        }
+
+        public void DeleteOrder(string name)                                               //根据名字删除订单
+        {
+            bool count = false;
+            for (int i = 0; i < orders.Count; i++)
+            {
+                if (name == orders[i].guestName || name == orders[i].goodsName)
+                {
+                    orders.Remove(orders[i]);
+                    count=true;
+                }
+            }
+            if (count == false)
+            { throw new CanNotFindOrder($"没有该订单！"); }
+        }
+
+
+        public void ChangeOrder(string oldNumber, string newName)                           //根据订单号修改顾客名
+        {
+            bool count = false;
+            foreach (var order in orders)
+            {
+                if (oldNumber == order.orderNumber)
+                {
+                    order.guestName= newName;
+                    count = true;
+                }
+            }
+            if (count == false)
+            { throw new CanNotFindOrder($"没有该订单！"); }
+        }
+
+        public void SearchOrderByNumber(string ordernum)                             //根据订单号进行查询(linq)
+        {
+            var selorder = from n in orders where n.orderNumber == ordernum select n;
+
+            foreach (var n in selorder)
+            {
+                    Console.Write(n.orderNumber + " " + n.guestName + " " + n.goodsName + " " + n.goodsPrice);
+                Console.WriteLine();
+            }
+            
+        }
+
+        public void SearchOrderByName(string name)                         //根据名查找订单(linq)
+        {
+            
+            var selorder = from n in orders where n.guestName == name || n.goodsName == name select n;
+            foreach (var n in selorder)
+            {
+                    Console.Write(n.orderNumber + " " + n.guestName + " " + n.goodsName + " " + n.goodsPrice);
+                Console.WriteLine();
+                    
+                }
+            }
+
+        public void ShowAllOrders()
+            {
+                int nCount = 1;
+                foreach (var od in orders)
+                {
+                    Console.WriteLine("订单" + nCount);
+                    od.ShowOrder();
+                    nCount++;
+                }
+            }
+        }
+    }
---
+++ homework5/homework5_2/homework5_2/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace homework5_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+            private void button1_Click(object sender, EventArgs e)
+            {
+                if (graphics == null) graphics = this.CreateGraphics();
+                drawCayleyTree(10, 200, 310, 100, -Math.PI / 2);
+            }
+        private Graphics graphics;
+        double th1 = 70 * Math.PI / 180;
+        double th2 = 50 * Math.PI / 180;
+        double per1 = 0.7;
+        double per2 = 0.5;
+
+        void drawCayleyTree(int n, double x0, double y0, double leng, double th)
+        {
+            if (n == 0) return;
+            double x1 = x0 + leng * Math.Cos(th);
+            double y1 = y0 + leng * Math.Sin(th);
+            drawLine(x1, y1, x0, y0);
+            drawCayleyTree(n - 1, x1, y1, per1 * leng, th + th1);
+            drawCayleyTree(n - 1, x1, y1, per2 * leng, th - th2);
+        }
+        void drawLine(double a, double b, double c, double d)
+        {
+            graphics.DrawLine(
+                Pens.Green, (int)a, (int)b, (int)c, (int)d);
+        }
+    }
+}
