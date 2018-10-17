019b4accd82fec5c597e40a63edae7ab021134fa
starrainjung <43143231+starrainjung@users.noreply.github.com>
2018-10-17 21:49:04 +0800

    改正第四次作业订单部分

---
+++ homework4_2/homework4_2/Order.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+   public  class Order
+    {
+        public string orderNumber { get; set; }
+       
+        public string guestName { get; set; }
+
+        public string goodsName { get; set; }
+        public double goodsPrice { get; set; }
+
+       
+
+        public Order(string orderNumber, string guestName, string goodsName, double goodsPrice)
+        {
+            this.orderNumber = orderNumber;
+            this.guestName = guestName;
+            this.goodsName = goodsName;
+            this.goodsPrice = goodsPrice;
+        }
+
+
+
+        public void ShowOrder()
+        {
+            Console.Write(orderNumber + " " + guestName + " " + goodsName + " " + goodsPrice);
+        }
+
+    }
+}
---
+++ homework4_2/homework4_2/Program2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order1 = new Order("001", "mina", "鞋",600);
+            Order order2 = new Order("002", "momo", "猪蹄", 20);
+            Order order3 = new Order("003", "jyh", "奶茶", 12);
+            Order order4 = new Order("004", "jyh", "鞋", 622);
+            Order order5 = new Order("005", "sana", "书", 48);
+            Order order6 = new Order("006", "sana", "帽子", 100);
+            OrderService Od = new OrderService();
+
+            Od.add(order1);                     //添加订单
+            Od.add(order2);
+            Od.add(order3);
+            Od.add(order4);
+            Od.add(order5);
+            Od.add(order6);
+
+
+            Console.WriteLine("订单改变前：");
+            Od.ShowAllOrders();
+
+            Console.WriteLine();
+
+            Console.WriteLine("搜索005号订单和搜索鞋的订单结果：");
+            Od.SearchOrderByNumber("005");                  //查找指定订单
+
+            Od.SearchOrderByName("鞋");
+
+            Console.WriteLine();
+
+            Console.WriteLine("订单改变后：");
+            Od.ChangeOrder("004", "chae");
+            
+            Od.DeleteOrderByOrderNumber("006");      
+            Od.ShowAllOrders();
+
+            Console.ReadKey();
+        }
+    }
+}
+        
